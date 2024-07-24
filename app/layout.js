0daf6cc9dda2adf4b8bdb528f9d0211354114fd0
Tejpal Singh <tejpalsinghroyalbanna12@gmail.com>
2024-07-24 17:19:52 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -17,10 +17,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${popins.className} bg-primary`}>
-        <GoogleOAuthProvider clientId="466584842768-mt9murppikq6an70siqrjdh9vlojptea.apps.googleusercontent.com"  >
 
         {children}
-        </GoogleOAuthProvider>
         
         </body>
     </html>
